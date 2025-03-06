b102039953a07ca9e75fdee4ae12f8020c71a0cd
khanhphan040204 <khanhphan.040204@gmail.com>
2025-03-06 15:17:25 +0700

    lab01 ex02

---
+++ Lab-01/ex02/ex02_04.py
@@ -0,0 +1,8 @@
+# tạo một danh sách rỗng để lưu kết quả
+j = []
+# duyệt qua tất cả các số trong đoạn từ 2000 đến 3200, kiểm tra xem số i có chia hết cho 7 không 
+# và không phải là bội số của 5 không 
+for i in range(2000, 3201):
+    if ( i % 7 ==0 ) and ( i % 5 != 0 ):
+        j.append(str(i))
+print(','.join(j))
